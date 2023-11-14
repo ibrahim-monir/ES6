d041a652828597b3a6cfb207c42263496da13bba
ibrahim-monir <ibrahimkhalilmp@gmail.com>
2023-11-14 14:40:36 +0600

    Spread Operator

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 console.log("Hello World");
 console.log('I Love Bangladesh');
 
+
+//Use Srict Mood
 "use strict";
 MyName();
 function MyName(){
     MyName=("Ibrahim Khalil");
 }
+
+//Spread Operator
+
+let developingCountry=['Bangladesh', 'Srilanka'];
+
+let richCountry=['America','China', 'Japan'];
+
+let richCountry2=[...developingCountry,'America','China','Japan'];
+
+let allCountry=[...developingCountry,...richCountry2];
+console.log(allCountry);
